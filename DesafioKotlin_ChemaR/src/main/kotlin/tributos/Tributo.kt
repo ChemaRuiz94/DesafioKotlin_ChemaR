1251a6ddfcec05fa0ec92840dbf27d822ea8bef0
Chema <chemaruiz94@gmail.com>
2021-09-30 14:28:42 +0200

    Commit - Clase Tributo - funciones

--- DesafioKotlin_ChemaR/src/main/kotlin/tributos/Tributo.kt
+++ DesafioKotlin_ChemaR/src/main/kotlin/tributos/Tributo.kt
@@ -2,5 +2,16 @@ package tributos
 
 class Tributo(var distrito: Int, var vida: Int, var fuerza: Int) {
 
+    fun aumentarVida(aumento: Int) {
+        this.vida = this.vida + aumento
+    }
+
+    fun restarVida(disminuacion: Int) {
+        this.vida = this.vida - disminuacion
+    }
+
+    fun aumentarFuerza(aumento: Int) {
+        this.fuerza = this.fuerza + aumento
+    }
 
 }
